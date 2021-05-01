1c6b64eee2118919ec9c26ac1bcffb181403f4d2
Rekha-15 <rekhapatil.1509@gmail.com>
2021-05-01 16:33:51 -0400

    add: FlipCoinsSimulation.sh

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,2 +1,9 @@
 #!/bin/bash -x
-echo "Welcome to Flip Coin Simulation"
+echo "Welcome to Flip Coin Simulation Problems"
+x=$(( RANDOM%2 ));
+if [ $x -eq 1 ];
+ then
+        echo "Head is winner" ;
+ else
+        echo "Tail is winner" ;
+fi
