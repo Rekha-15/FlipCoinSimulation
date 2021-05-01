73a1e4551aba532d2764c3a9328c3284dc99464b
Rekha-15 <rekhapatil.1509@gmail.com>
2021-05-01 16:39:18 -0400

    add: FlipCoinSimulation.sh

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,2 +1,22 @@
 #!/bin/bash -x
 echo "Welcome to Flip Coin Simulation"
+#!/bin/bash -x
+
+echo "WELCOME TO FLIP COIN SIMULATION PROBLEM"
+
+head=0;
+tail=0;
+read -p "Enter number of flip required" n
+for (( i=0; i<n; i++ ))
+do
+
+	x=$(( RANDOM%2 ));
+	if [ $x -eq 1 ]
+	then
+         	head=$(($head+1))
+	else
+        	tail=$(($tail+1))
+	fi
+done
+echo "Number of head win" $head;
+echo "Number of tail win" $tail;
